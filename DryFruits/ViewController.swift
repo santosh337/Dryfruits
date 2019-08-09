8bd2cce5ea5220da1afe75f4cb8bf99c63702d91
Santosh Sugur <santoshk@jazeeratechnologies.com>
2019-08-09 12:44:51 +0530

    Initial Commit

---
+++ DryFruits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DryFruits
+//
+//  Created by Santosh  on 09/08/19.
+//  Copyright © 2019 Santosh . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
