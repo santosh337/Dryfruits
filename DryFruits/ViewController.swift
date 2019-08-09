46ce974a78415371aae985456835a3d9f427daae
santosh337 <santosh.iosdev@gmail.com>
2019-08-09 13:00:07 +0530

    first and initial stage commit

--- DryFruits/ViewController.swift
+++ DryFruits/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Santosh added the merge file to the window")
         // Do any additional setup after loading the view.
     }
 
