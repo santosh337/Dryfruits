81a05c81cc0e89db2f6b634a7c5fa26cd1f51915
santosh337 <santosh.iosdev@gmail.com>
2019-08-31 18:23:39 +0530

    second changes

--- DryFruits/ViewController.swift
+++ DryFruits/ViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /** Create the UILabel */
+    var showLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.textAlignment = .center
+        label.text = "Santosh have created the text and will use it"
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.sizeToFit()
+        label.font = UIFont(name: "Helvetica-Bold", size: 16)
+        return label
+    }()
+    
+    var switchBtn : UIButton = {
+        var button = UIButton()
+        button.setTitle("To Do Add", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.backgroundColor = button.isHighlighted ? UIColor.blue : UIColor.red
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Santosh added the merge file to the window")
+        showLabel.frame = CGRect(x: 30, y: self.view.center.y, width: view.frame.width - 60, height: 40)
+        switchBtn.frame = CGRect(x: 30, y: self.view.frame.origin.y + 70, width: view.frame.width - 60, height: 30)
+        self.view.addSubview(switchBtn)
+        self.view.addSubview(showLabel)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func buttonPressed() {
+    print("Items are set selected")
     }
     @IBAction func clicked(_ sender: UIButton) {
         print("Button was clicked but not done anything for it")
     }
+    override func viewDidLayoutSubviews() {
+        /* Set the frame when the layout is changed */
+        
+    }
     
 
 }
+
 
